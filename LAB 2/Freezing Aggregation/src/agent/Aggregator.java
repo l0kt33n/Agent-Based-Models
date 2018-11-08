9de208d8eb6e702a5c7ac2711a0ca5dfc88f724a
l0kt33n <aa77855@gmail.com>
2018-11-07 17:56:45 -0800

    Added new getters

--- LAB 2/Freezing Aggregation/src/agent/Aggregator.java
+++ LAB 2/Freezing Aggregation/src/agent/Aggregator.java
@@ -1,7 +1,7 @@
-package agents;
+package agent;
+
 import sim.engine.SimState;
 import sim.engine.Steppable;
-import sim.field.grid.Grid2D;
 import sim.field.grid.SparseGrid2D;
 import sim.util.Bag;
 import simulation.FreezingSim;
@@ -11,7 +11,6 @@ public class Aggregator implements Steppable {
 
 	protected int x,y,dirx,diry;
 	protected boolean frozen;
-	protected boolean atEdge;
 	protected FreezingSim sim;
 	
 	
@@ -33,37 +32,14 @@ public class Aggregator implements Steppable {
 	}
 	protected void move() {
 		SparseGrid2D space = sim.acquireSpace();
-		int tempx = x + dirx;
-		int tempy = y + diry;
+		int tempx = space.stx(x + dirx);
+		int tempy = space.stx(y + diry);
 		//Bag b = space.getObjectsAtLocation(tempx, tempy);
-		
-			if(this.edgeCheck(sim, tempx, tempy))
-			{
-				if(sim.isBounded())
-				{
-					dirx = -dirx;
-					diry = -diry;
-					tempx = x+dirx;
-					tempy = y+dirx;
-				}
-				else
-				{
-					tempx = space.stx(x+ dirx);
-					tempy = space.sty(y+ diry);
-				}
-				x=tempx;
-				y=tempy;
-				space.setObjectLocation(this, x, y);
-			}
-			
-		
-		
-		if(this.aggregateCheck(space, tempx, tempy)) {
+		if(aggregate(space, tempx, tempy)) {
 			frozen = true;
 			dirx = 0;
 			diry = 0;
 		}
-		
 		else {
 		x = tempx;
 		y = tempy;
@@ -73,7 +49,7 @@ public class Aggregator implements Steppable {
 		return;
 	}
 	
-	public boolean aggregateCheck(SparseGrid2D space, int tempx, int tempy){
+	public boolean aggregate(SparseGrid2D space, int tempx, int tempy){
 		Bag b = space.getObjectsAtLocation(tempx, tempy);
 		if(b!=null) {
 			Aggregator nextObj = (Aggregator) b.objs[0];
@@ -83,29 +59,9 @@ public class Aggregator implements Steppable {
 			return false;
 	}
 	
-	public boolean edgeCheck(FreezingSim sim, int tempx, int tempy)
-	{
-		if(tempx==0||tempy==0)
-		{
-			return true;
-		}
-		else if(tempx==sim.getGridHeight()-1||tempy==sim.getGridWidth()-1)
-		{
-			return true;
-		}
-		
-		return false;
-	}
-	
-	
-	protected Bag returnBag(int r)
-	{
-		Bag neighbhors = sim.acquireSpace().getMooreNeighbors(x, y, r, Grid2D.BOUNDED, true);
-		return neighbhors; 
-	}
 	protected void randomizeMovement() {
 		if(this.frozen == false) {
-			if (sim.random.nextInt(100) < (sim.getP() * 100)){
+			if (sim.random.nextInt(100) > (sim.getP() * 100)){
 
 				dirx = sim.random.nextInt(3) - 1;
 
@@ -114,6 +70,72 @@ public class Aggregator implements Steppable {
 			}
 		}
 	}
-}
+	
+	public double getDistanceFromCenter() {
+		double dx = (double)x - sim.getGridWidth()/2;
+		double dy = (double)y - sim.getGridHeight()/2;
+		return Math.sqrt(dx*dx+dy*dy);
+	}
+	
+	public int getNNeighbors() {
+		SparseGrid2D sp = sim.acquireSpace();
+		int mode = SparseGrid2D.BOUNDED;
+		if(sim.isBounded()) {
+			mode = SparseGrid2D.TOROIDAL;
+		}
+		Bag b = sp.getMooreNeighbors(x, y, 1, mode, true);
+		if (b==null) {
+			return 0;
+		}
+		return b.numObjs;
+		}
 
-// writeup: change grid size, N, toroidal vs. bounded, probabliltiy
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getDirx() {
+		return dirx;
+	}
+
+	public void setDirx(int dirx) {
+		this.dirx = dirx;
+	}
+
+	public int getDiry() {
+		return diry;
+	}
+
+	public void setDiry(int diry) {
+		this.diry = diry;
+	}
+
+	public boolean isFrozen() {
+		return frozen;
+	}
+
+	public void setFrozen(boolean frozen) {
+		this.frozen = frozen;
+	}
+
+	public FreezingSim getSim() {
+		return sim;
+	}
+
+	public void setSim(FreezingSim sim) {
+		this.sim = sim;
+	}
+	
+}
